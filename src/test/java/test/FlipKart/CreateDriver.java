2fb75668a85f2ebf5b1029e55da9a43b02f4992b
Sachin <sachingainewar@gmail.com>
2020-01-20 12:29:41 +0100

    Added Property file to store Auth0Key and other environment veriable

--- src/test/java/test/FlipKart/CreateDriver.java
+++ src/test/java/test/FlipKart/CreateDriver.java
@@ -16,7 +16,64 @@ public class CreateDriver extends AppiumServerJava{
 	private static final ThreadLocal<AndroidDriver> mobileDriver = new ThreadLocal<AndroidDriver>();
 	private URL serverUrl = null;
 	
+	///Code to initialized the Browserstake  
+		/**
+	 * Method to create driver in Browserstake  
+	 * @param capabilities
+	 * @param testBedConfig.
+	 NOTE:  Do not forgot to add "BrowserStackLocal.exe " in folder.
+	 */
+	
+	static Common getMethod = new Common();
+	public static RemoteWebDriver driver;
+	static Properties properties = new Properties();
+	static int count = 0;
+	public static final String USERNAME = "sachin510";
+	public static final String AUTOMATE_KEY = properties.getProperty("AuthKey");
+	public static final String URL = "https://" + USERNAME + ":" + AUTOMATE_KEY + "@hub-cloud.browserstack.com/wd/hub";
+	static Delay delay = new Delay();
 
+	
+	public void setConnectionToBroserStack() throws InterruptedException, ExecuteException, IOException {
+		// Initializing command line parameters...
+
+		CommandLine cmdLine = new CommandLine("cmd.exe");
+		cmdLine.addArgument(
+				"C:\\Users\\sgainewar>\\Documents\\new\\9-1-2018\\BrowserStackLocal.exe --key zKpP3Muz6StvsebgqUqm");
+		/*
+		 * cmdLine.addArgument("--key");
+		 * cmdLine.addArgument("zKpP3Muz6StvsebgqUqm");
+		 */
+		System.out.println(cmdLine);
+		DefaultExecutor executor = new DefaultExecutor();
+		int exitValue = executor.execute(cmdLine);
+		System.out.println(exitValue);
+	}
+
+		// To load the property file
+	@BeforeSuite
+	public void loadPropertyFile() {
+		FileInputStream fin = null;
+		try {
+			fin = new FileInputStream(System.getProperty("user.dir")
+					+ "//resources//GlobalElementDetails.properties");
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			throw new CommonException("File Not found at the specified path");
+		}
+		try {
+			properties.load(fin);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+	
+	
+	
+	
+//**************************************LOCAL DRIVER******************	
 	
 	/**
 	 * Method to create local driver
